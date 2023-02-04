2987db5c7acdcc58685f9d50ece7a1772f4e935e
Jordan <hansejor008@gmail.com>
2023-02-04 13:48:36 -0700

    The rest of them

---
+++ iTunesSearch 2/iTunesSearch/ItemCollectionViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell, ItemDisplaying {
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    
+}
---
+++ iTunesSearch/iTunesSearch/ItemCollectionViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell, ItemDisplaying {
+    
+    @IBOutlet var itemImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
+    
+}
